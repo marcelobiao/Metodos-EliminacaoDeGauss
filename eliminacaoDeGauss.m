3227de521186d8db95d161b3d22f7c2fa0300aad
marcelobiao <marcelobiao2@gmail.com>
2017-04-30 12:00:36 -0300

    Corrigindo eliminacaodegauss

--- eliminacaoDeGauss.m
+++ eliminacaoDeGauss.m
@@ -2,17 +2,25 @@ function raizes = eliminacaoDeGauss(A,b)
     [m,n] = size(A);
     Ab = [A b];
     for k=1:n-1
-        %parte do pivoteamento
+        %Pivoteamento
+        %Retorna o maior numero da coluna e escolhe o pivo
         [~,p] = max(abs(Ab(k:n,k)));
+        
+        %Posiciona o Pivo corretamente
         p = p+k-1;
         if (p ~= k)
-            % pivoteamento das linhas
             Ab([k,p],:)=Ab([p,k],:);
         end
-        for (i=k+1:n)
+        
+        %Zera valores abaixo do pivo
+        for (i=k+1:n)            
             m = Ab(i,k)/Ab(k,k);
             Ab(i,k:end) = Ab(i,k:end)-m*Ab(k,k:end);
         end
+        fprintf('Iteração k=%d\n',k);
+        disp(Ab);
     end
+    
+    %Resolvendo o sistema linear
     raizes = backsub(Ab(:,1:n),Ab(:,end));
 end
