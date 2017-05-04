9ee402ab61289cfadfc51da594f8d6c0a558626e
marcelobiao <marcelobiao2@gmail.com>
2017-05-03 22:59:35 -0300

    Implementando resolucao de sistemas lineares - triangulares inferiores

--- backinf.m
+++ backinf.m
@@ -1,14 +1,12 @@
 function X=backinf(A,B)
-%Input  - A is an n x n upper-triangular nonsingular matrix
+%Input  - A is an n x n down-triangular nonsingular matrix
 %	     - B is an n x 1 matrix
 %Output - X is the solution to the linear system AX = B
 
  n=length(B);
  X=zeros(n,1);
- X(n)=B(n)/A(n,n);
+ X(1)=B(1)/A(1,1);
 
-for k=1:n:-1
-    fprintf('aqui\n');
-    disp(k);
- X(k)=(B(k)-A(k,k+1:n)*X(k+1:n))/A(k,k);
+for k=2:1:n
+    X(k)=(B(k)-A(k,1:k)*X(1:k))/A(k,k);
 end
